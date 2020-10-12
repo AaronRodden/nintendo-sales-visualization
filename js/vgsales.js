b2ab53907d92e0626ca2d1478eb464e5dccae380
unknown <aaron.rodden21@gmail.com>
2020-10-11 20:49:53 -0700

    Images on the side and rest of elements scaled / placed properly

--- js/vgsales.js
+++ js/vgsales.js
@@ -4,9 +4,11 @@ const WIDTH = 800;
 const HEIGHT = 300;
 const TITLE_X_OFFSET = 750;
 const TITLE_Y_OFFSET = 20;
+// TODO: Fix this hardcoding
+let IMAGE_X_OFFSET = 276;
+let IMAGE_Y_OFFSET = 0;
 
-function createBarChart(chartData, key, aggregates, scaleObj) {
-  // console.log(chartData);
+function createBarChart(chartData, key, div, aggregates, scaleObj) {
 
   var data = [];
   for (let i = 0; i < chartData.length; i++) {
@@ -15,8 +17,6 @@ function createBarChart(chartData, key, aggregates, scaleObj) {
   }
 
   // console.log(data);
-
-  let div = d3.select('.nintendo-viz').append('div').classed(key,true);
 
   var svg = d3.select(`.${key}`)
             .append("svg")
@@ -31,19 +31,9 @@ function createBarChart(chartData, key, aggregates, scaleObj) {
       class : "bar",
       width : function(d) {return scaleObj["scale"](d)},
       height: "40",
-      y : function(d, i) {return i*50 + 10;},
+      y : function(d, i) {return i*50 + 50;},
       x : "10"
      });
-
-  //TODO: Title text location is hardcoded
-  svg.append("text")
-      .attr("x", WIDTH - TITLE_X_OFFSET)
-      .attr("y", HEIGHT - TITLE_Y_OFFSET)
-      .attr("text-anchor", "middle")
-      .style("font-size", "16px")
-      .style("text-decoration", "underline")
-      .text(chartData[0]["Platform"]);
-
 }
 
 function gatherAggregates(json) {
@@ -67,15 +57,12 @@ function gatherAggregates(json) {
 function createAxis(aggregates) {
   // Create scale
   let scaleSvg = d3.select('.nintendo-viz').append('svg').classed("global-axis", true)
-                .attr("width", WIDTH+100)
+                .attr("width", WIDTH)
                 .attr("height", 30)
-                .attr("x", 0)
 
   var scale = d3.scale.linear()
                 .domain([d3.min(aggregates["Global_Sales"]), d3.max(aggregates["Global_Sales"])])
                 .range([10, d3.max(aggregates["Global_Sales"])*10]);
-
-  // console.log("Scale test: " + scale(3.6));
 
   // Add scales to axis
   var x_axis = d3.svg.axis(scale)
@@ -83,6 +70,7 @@ function createAxis(aggregates) {
 
   //Append group and insert axis
   scaleSvg.append("g")
+     .attr("transform", "translate(" + IMAGE_X_OFFSET + "," + (10) + ")")
      .call(x_axis);
 
   return {
@@ -91,11 +79,22 @@ function createAxis(aggregates) {
   };
 }
 
+//TODO: Should an image be a seperate div?
 function insertPicture(key) {
-  var img = document.createElement("img"); 
- 
+  var img = document.createElement("img", {class : `${key}-img`});
+
   img.src = `assets/${key}/${key}.png`; 
-  var src = document.getElementsByClassName("nintendo-viz")[0]; 
+  // img.addEventListener('load', function () {
+  //   // console.log("It's loaded!")
+  // })
+  // img.onload = function() {
+  //   console.log("Width: " + this.width);
+  //   console.log("Height: " + this.height);
+  //   IMAGE_X_OFFSET = this.width;
+  //   IMAGE_Y_OFFSET = this.height;
+  // }
+
+  var src = document.getElementsByClassName(`${key}`)[0];
    
   src.appendChild(img); 
 }
@@ -105,15 +104,13 @@ function readDataFiles() {
   $.getJSON("data/nintendo.json", function(json) {
     console.log(json);
 
-    // createBarChart(json["Wii"]);
-
     let aggregates = gatherAggregates(json);
-
     let scaleObj = createAxis(aggregates);
 
     for (let key in json){
-      insertPicture(key);
-      createBarChart(json[key], key, aggregates, scaleObj);
+      let div = d3.select('.nintendo-viz').append('div').classed(key,true);
+      insertPicture(key, div);
+      createBarChart(json[key], key, div, aggregates, scaleObj);
     }
   });
 }
